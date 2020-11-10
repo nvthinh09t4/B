ac150194651e70b5d3481e6b5e20d1dc3a0a3bfe
nguyen van thinh <nvthinh09t4@outlook.com>
2020-11-11 01:11:13 +0700

    add

---
+++ B/Middleware/MiddlewareExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace B.Middleware
+{
+    public static class MiddlewareExtension
+    {
+        public static IWebHostBuilder UseFailing(this IWebHostBuilder builder, Action<FailingOptions> options)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<IStartupFilter>(new FailingStartupFilter(options));
+            });
+            return builder;
+        }
+    }
+}
