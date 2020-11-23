0cbce185afa91d3fd38c4486e19c80d25df1eb81
nvthinh09t4 <nguyenvanthinh@vnpt.vn>
2020-11-23 19:15:13 +0700

    aaaaaaaaaaa

---
+++ sln/AspCoreMvc5/Data/Speaker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspCoreMvc5.Data
+{
+    public class Speaker
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public string Description { get; set; }
+        public string ShortQuote { get; set; }
+    }
+}
