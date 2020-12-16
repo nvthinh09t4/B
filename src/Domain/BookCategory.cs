6390f261da9b6e92685d5ebe8d1a3932437a67a0
nguyen van thinh <nvthinh09t4@outlook.com>
2020-12-16 23:59:27 +0700

    hehe

---
+++ src/Domain/BookCategory.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class BookCategory : BaseEntity
+    {
+        public string Type { get; set; }
+    }
+}
