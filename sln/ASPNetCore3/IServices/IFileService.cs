d087f80d45e0fa4c7eb8bffa08788d6f87682889
nguyen van thinh <nvthinh09t4@outlook.com>
2021-03-06 14:10:37 +0700

    zzz

---
+++ sln/ASPNetCore3/IServices/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore3.IServices
+{
+    public interface IFileService
+    {
+        Task<FileStorage> SaveFile();
+        Task<byte[]> DownloadFile();
+    }
+}
---
+++ sln/ASPNetCore3/ServiceImpl/FileService.cs
@@ -0,0 +1,22 @@
+﻿using ASPNetCore3.IServices;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCore3.ServiceImpl
+{
+    public class FileService : IFileService
+    {
+        public Task<byte[]> DownloadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileStorage> SaveFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
